873ba36800fd4b4f66f81f21e34f6716428a27e9
Sett Sarverott <sarverott@gmail.com>
2024-01-03 09:20:18 +0100

    making cli looks pretty, spontanious feachers add

--- _index.js
+++ _index.js
@@ -19,11 +19,14 @@ const STANDARD_HEADER=`
 |     ░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╦╝╚█████╔╝██╔╝╚██╗     |
 |___________________________________________________________________|
 \x1b[0m
+`
 
+const REPOS_INFO=`
 \t    \x1b[41mNPM:\x1b[0m \x1b[31mhttps://www.npmjs.com/package/carnival-toolbox\x1b[0m
 \t \x1b[46mGitHub:\x1b[0m \x1b[36mhttps://github.com/Sarverott/carnival-toolbox\x1b[0m
 
-\t\t\t \x1b[2m~~~ Sarverott @ 2024 ~~~\x1b[0m
+\t\t\t \x1b[2m~~~ Sett Sarverott @ 2024 ~~~\x1b[0m
+\t\t \x1b[2m~~~ published on terms of MIT license ~~~\x1b[0m
 `
 
 const HELP_LEGEND=`
@@ -37,23 +40,72 @@ it's sandboxed code evaluator, so JS is also accepted.
 
 var PROMPT=null;
 
+//umActually.primeNums("result correct it is not", "opposite site of force it is", "problem i see")
+
+class GeekyDodgeRaportRawr{
+  static get RAPORT(){
+    if(
+      !GeekyDodgeRaportRawr.hasOwnProperty(
+        "RAPORT_CHOIR"
+      )
+    )GeekyDodgeRaportRawr.RAPORT_CHOIR=[];
+    return [...GeekyDodgeRaportRawr.RAPORT_CHOIR];
+  }
+  static set RAPORT(dataIn){
+    if(
+      !GeekyDodgeRaportRawr.hasOwnProperty(
+        "RAPORT_CHOIR"
+      )
+    )GeekyDodgeRaportRawr.RAPORT_CHOIR=[];
+    GeekyDodgeRaportRawr.RAPORT_CHOIR.push(dataIn);
+  }
+  constructor(whatTheBark){
+    this.TARGET=whatTheBark;
+  }
+  complainDescription(...almightWisdomOfTheNerd){
+    console.log("BEGIN BARK", this.TARGET)
+    for(var i of almightWisdomOfTheNerd){
+      console.log("\x1b[0m\x1b[5m<<<BARK<<<\x1b[0m\x1b[4m", i)
+    }
+    console.log("\x1b[0mEND BARK", this.TARGET)
+    this.BARKLLADA=almightWisdomOfTheNerd;
+    console.log("bug recorded to local debugger variable!")
+  }
+  export(){}
+}
+
+var GDRR=GeekyDodgeRaportRawr;
+
 function NEW_PROMPT(){
+
   Object.assign(TOOLBOX, {
     exit(){
       PROMPT.close();
     },
+    umActually:new Proxy(GDRR, {
+      get(target,name){
+        var woff=new GeekyDodgeRaportRawr(name)
+        GeekyDodgeRaportRawr.RAPORT=woff;
+        return function(...allData){
+          woff.complainDescription(...allData);
+        }
+      }
+    }),
     PROMPT:PROMPT,
-    process:process
-  });
+    process:process,
+    GeekyDodgeRaportRawr:GeekyDodgeRaportRawr,
+    GDRR:GDRR
+  });//umActually.primeNumber("result correct it is not")
   Object.defineProperty(TOOLBOX, "exit", {
     get(){
       PROMPT.close();
       return null;
     }
-  })
+  });
   Object.defineProperty(TOOLBOX, "help", {
     get(){
-      console.log(STANDARD_HEADER);
+      console.log("\t\t # # # ABOUT CARNIVAL TOOLBOX # # #");
+      console.log(REPOS_INFO);
       console.log(HELP_LEGEND);
       //var descriptor={
       //  lvl:"library",
@@ -75,7 +127,7 @@ function NEW_PROMPT(){
   return readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: "|~\x1b[44m\x1b[31m ▓▒░ \x1b[0m \x1b[7m CARNIVAL TOOLBOX \x1b[0m \x1b[44m\x1b[31m ░▒▓ \x1b[0m~>"
+    prompt: "|~\x1b[44m\x1b[31m ▓▒░ \x1b[0m \x1b[7m CT :: Sandbox lab \x1b[0m \x1b[44m\x1b[31m ░▒▓ \x1b[0m~>"
   }).on(
     'line',
     ON_PROMPT_LINE
@@ -94,8 +146,9 @@ function ON_PROMPT_CLOSED(){
   }
   outputTMP+='\x1b[0m';
   console.log(outputTMP);
+  console.log(REPOS_INFO);
   console.log(STANDARD_HEADER);
-  console.log('Thank you for testing/using/depending/requiring!');
+  console.log('\tThank you for testing/using/depending/requiring!');
   console.log("\t\t\tBYE!");
   process.exit(0);
 }
@@ -126,12 +179,26 @@ function ON_PROMPT_LINE(line){
 
   const inputScript = new vm.Script(line);
   vm.createContext(TOOLBOX);
-  //for (let i = 0; i < 10; ++i) {
+  //for (let i = 0; i < 10; ++i) {var textTMP=" user-friendly escape sequence of programm... ";
+  //textTMP=" . . . ";
+  var outputTMP=' ';
+  for(var i=0;i<5;i++){
+    outputTMP+=`\x1b[3${Math.floor(Math.random()*5%6+1)}m`;
+    outputTMP+='- ';
+  }
+  console.log(outputTMP+'\x1b[0m');
+  //console.log("\x1b[2m . . . \x1b[0m");
   try{
     console.log(inputScript.runInContext(TOOLBOX));
   }catch(e){
     console.log(e);
   }
+  outputTMP=' ';
+  for(var i=0;i<5;i++){
+    outputTMP+=`\x1b[3${Math.floor(Math.random()*6%6+1)}m`;
+    outputTMP+='- ';
+  }
+  console.log(outputTMP+'\x1b[0m');
 
 
   //}
@@ -143,6 +210,8 @@ function functionName() {
 }
 
 if(require.main===module){
+  console.log(STANDARD_HEADER);
+  console.log(REPOS_INFO);
   PROMPT=NEW_PROMPT();
   PROMPT.prompt();
 }else{
