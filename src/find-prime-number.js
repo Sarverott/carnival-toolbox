e58bd7bc96aeaa674ca8637f9f956bbbe1d24344
Sett Sarverott <sarverott@gmail.com>
2024-01-03 02:52:17 +0100

    old gist with prime number finding function added
    //https://gist.github.com/Sarverott/c37b983be2253882844c4b010bd9e240

---
+++ src/find-prime-number.js
@@ -0,0 +1,29 @@
+/*
+prime number finder
+Sett Sarverott 2022
+*/
+//https://gist.github.com/Sarverott/c37b983be2253882844c4b010bd9e240
+function primeNumber(
+  endingNumber=100,
+  withFinderShift=false
+){
+  var ret=[];
+  var i=0;
+  while(endingNumber-(++i)){
+    var flag=true;
+    for(var j in ret)
+      flag=(i%ret[j])?(false:flag);
+    if(flag){
+      ret.push(i);
+      if(withFinderShift)
+        endingNumber++;
+    }
+  }
+  if(withFinderShift)
+    ret={
+      limit:endingNumber,
+      array:ret
+    };
+  return ret;
+}
+module.exports=primeNumber;
