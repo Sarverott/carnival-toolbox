6e466ae16182a9f22c96c30471af626891223592
Sett Sarverott <sarverott@gmail.com>
2024-01-05 03:35:20 +0100

    anti-anti-prime number rewriting of primeNum
    old version results with power of two, starting with 2**0 - i don't remember, it's old code

--- src/_index.js
+++ src/_index.js
@@ -2,7 +2,7 @@ module.exports={
   jsonExt:require("./json-gadgets.js"),
   caseChange:require("./case-gadgets.js"),
   randomString:require('./random-string.js'),
-  findPrimeNumbers:require('./find-prime-number.js'),
+  primeNum:require('./find-prime-number.js'),
   musicaledCiphers:require('./music-easteregged-cryptography.js'),
   Fibonacci:require('./fibonacci.js')
 }
--- src/find-prime-number.js
+++ src/find-prime-number.js
@@ -3,6 +3,7 @@ prime number finder
 Sett Sarverott 2022
 */
 //https://gist.github.com/Sarverott/c37b983be2253882844c4b010bd9e240
+/*
 function primeNumber(
   endingNumber=100,
   withFinderShift=false
@@ -13,8 +14,8 @@ function primeNumber(
     var flag=true;
     for(var j in ret)
         if(i%ret[j])flag=false;
-    
-    if(flag){
+
+    if(!flag){
       ret.push(i);
       if(withFinderShift)
         endingNumber++;
@@ -26,5 +27,34 @@ function primeNumber(
       array:ret
     };
   return ret;
+}
+*/
+function primeNumber(limit, mode="reach"){ //modes: reach | count
+  /*
+  algorithm:
+  -preset empty primes array and one in testednumber
+  -increment the testednumber ###looping jump here<<<
+  -repeat for all in array is testednumber modulo prime equal zero
+    ---[OPTIMALISER]end if primes starts to be greater than half of testednumber - skip impossible to return true tests
+  -if all tests false result add testednumber value to primes (just keeping in mind obviouses: first loop has zero tests, it can't ligicjokering)
+  -repeat looping until reach wanted count or to given number
+  */
+  var result=[];
+  var testNum=0;
+  var i=0;
+  while(i<limit){
+    testNum++
+    var testflag=true;
+    for(var j=1;j<result.length&&testflag;j++)
+      testflag=testNum%result[j]!=0
+    if(testflag){
+      result.push(testNum);
+      if(mode=="count")
+        i++
+    }
+    if(mode=="reach")
+      i++;
+  }
+  return result;
 }
 module.exports=primeNumber;
