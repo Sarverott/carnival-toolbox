468db9d7f3dfeb2e7c8857f0e6924b811cc39a72
Sett Sarverott <sarverott@gmail.com>
2024-01-11 03:41:38 +0100

    starting writting dev-scripts/main.py

--- dev-scripts/main.py
+++ dev-scripts/main.py
@@ -1,2 +1,9 @@
-print("devtool is ready to use!")
-print("JK, it's not ready yet...")
+#print("devtool is ready to use!")
+#print("JK, it's not ready yet...")
+
+import os
+import sys
+import git
+
+from github import Github
+from github import Auth
